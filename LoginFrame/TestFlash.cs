a5fb7bafcacf5aa208a54bccdce6c563d46172ac
LancCJ <LancCJ@163.com>
2015-08-11 10:37:40 +0800

    修改

---
+++ LoginFrame/TestFlash.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Windows.Forms;
+
+namespace LoginFrame
+{
+    public partial class TestFlash : Form
+    {
+        public TestFlash()
+        {
+            InitializeComponent();
+
+           
+        }
+    }
+}
