fe9b5f592be32993999e10990fc8ebb43465c2f1
89266557@qq.com <89266557@qq.com>
2015-08-12 18:36:22 +0800

    Revert "Revert "Revert "111"""
    This reverts commit 60231be6fd465064cbaf3e167f2cab0203729949.

---
+++ Model/SYS_USER.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class SYS_USER
+    {
+        private int userId;
+        public int UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        private string userType;
+        public string UserType
+        {
+            get { return userType; }
+            set { userType = value; }
+        }
+
+        private string pwd;
+        public string Pwd
+        {
+            get { return pwd; }
+            set { pwd = value; }
+        }
+    }
+}
