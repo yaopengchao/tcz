21bb09509ebd2a68b74378db50c736a6eb817e7d
89266557@qq.com <89266557@qq.com>
2015-08-15 11:01:48 +0800

    修改密码页面搭建

---
+++ LoginFrame/ModifyPassword.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace LoginFrame
+{
+    partial class ModifyPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labTitle = new System.Windows.Forms.Label();
+            this.labUname = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtOldPwd = new System.Windows.Forms.TextBox();
+            this.txtNewPwd = new System.Windows.Forms.TextBox();
+            this.txtNewConfPwd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labTitle
+            // 
+            this.labTitle.AutoSize = true;
+            this.labTitle.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labTitle.Location = new System.Drawing.Point(30, 27);
+            this.labTitle.Name = "labTitle";
+            this.labTitle.Size = new System.Drawing.Size(124, 27);
+            this.labTitle.TabIndex = 4;
+            this.labTitle.Text = "修改密码";
+            // 
+            // labUname
+            // 
+            this.labUname.AutoSize = true;
+            this.labUname.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labUname.Location = new System.Drawing.Point(85, 90);
+            this.labUname.MinimumSize = new System.Drawing.Size(0, 30);
+            this.labUname.Name = "labUname";
+            this.labUname.Size = new System.Drawing.Size(66, 30);
+            this.labUname.TabIndex = 5;
+            this.labUname.Text = "原密码";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(85, 157);
+            this.label2.MinimumSize = new System.Drawing.Size(0, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "新密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(85, 221);
+            this.label3.MinimumSize = new System.Drawing.Size(0, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "确认密码";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 30);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtOldPwd
+            // 
+            this.txtOldPwd.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtOldPwd.Location = new System.Drawing.Point(227, 86);
+            this.txtOldPwd.MaximumSize = new System.Drawing.Size(300, 30);
+            this.txtOldPwd.MinimumSize = new System.Drawing.Size(300, 30);
+            this.txtOldPwd.Name = "txtOldPwd";
+            this.txtOldPwd.PasswordChar = '*';
+            this.txtOldPwd.Size = new System.Drawing.Size(300, 30);
+            this.txtOldPwd.TabIndex = 12;
+            this.txtOldPwd.Leave += new System.EventHandler(this.txtOldPwd_Leave);
+            // 
+            // txtNewPwd
+            // 
+            this.txtNewPwd.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtNewPwd.Location = new System.Drawing.Point(227, 153);
+            this.txtNewPwd.MaximumSize = new System.Drawing.Size(300, 30);
+            this.txtNewPwd.MinimumSize = new System.Drawing.Size(300, 30);
+            this.txtNewPwd.Name = "txtNewPwd";
+            this.txtNewPwd.PasswordChar = '*';
+            this.txtNewPwd.Size = new System.Drawing.Size(300, 30);
+            this.txtNewPwd.TabIndex = 13;
+            this.txtNewPwd.Leave += new System.EventHandler(this.txtNewPwd_Leave);
+            // 
+            // txtNewConfPwd
+            // 
+            this.txtNewConfPwd.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtNewConfPwd.Location = new System.Drawing.Point(227, 216);
+            this.txtNewConfPwd.MaximumSize = new System.Drawing.Size(300, 30);
+            this.txtNewConfPwd.MinimumSize = new System.Drawing.Size(300, 30);
+            this.txtNewConfPwd.Name = "txtNewConfPwd";
+            this.txtNewConfPwd.PasswordChar = '*';
+            this.txtNewConfPwd.Size = new System.Drawing.Size(300, 30);
+            this.txtNewConfPwd.TabIndex = 14;
+            this.txtNewConfPwd.Leave += new System.EventHandler(this.txtNewConfPwd_Leave);
+            // 
+            // ModifyPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.txtNewConfPwd);
+            this.Controls.Add(this.txtNewPwd);
+            this.Controls.Add(this.txtOldPwd);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labUname);
+            this.Controls.Add(this.labTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ModifyPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ModifyPassword";
+            this.Load += new System.EventHandler(this.ModifyPassword_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label labTitle;
+        public System.Windows.Forms.Label labUname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TextBox txtOldPwd;
+        public System.Windows.Forms.TextBox txtNewPwd;
+        public System.Windows.Forms.TextBox txtNewConfPwd;
+    }
+}
---
+++ LoginFrame/ModifyPassword.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoginFrame
+{
+    public partial class ModifyPassword : Form
+    {
+        public MainFrame mainFram;        
+
+        private Boolean oldPwdValid = false;
+
+        private Boolean newValid = false;
+
+        private Boolean newConfValid = false;
+
+        public ModifyPassword()
+        {
+            InitializeComponent();
+            if (mainFram == null)
+            {
+                mainFram = new MainFrame();
+            }
+        }
+
+        private void ModifyPassword_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtOldPwd_Leave(object sender, EventArgs e)
+        {
+            string oldPwd = txtOldPwd.Text;
+            if (oldPwd == null || oldPwd.Equals(""))
+            {
+                oldPwdValid = false;
+                MessageBox.Show("原密码不能为空");
+            } else
+            {
+                oldPwdValid = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (oldPwdValid && newValid && newConfValid)
+            {
+                MessageBox.Show("验证成功");
+            } else
+            {
+                MessageBox.Show("验证失败");
+            }
+        }
+
+        private void txtNewPwd_Leave(object sender, EventArgs e)
+        {
+            string newPwd = txtNewPwd.Text;
+            if (newPwd == null || newPwd.Equals(""))
+            {
+                newValid = false;
+                MessageBox.Show("新密码不能为空");
+            }
+            else
+            {
+                newValid = true;
+            }
+        }
+
+        private void txtNewConfPwd_Leave(object sender, EventArgs e)
+        {
+            string newConfPwd = txtNewConfPwd.Text;
+            if (newConfPwd == null || newConfPwd.Equals(""))
+            {
+                newConfValid = false;
+                MessageBox.Show("确认密码不能为空");
+            }
+            else
+            {
+                newConfValid = true;
+            }
+        }
+    }
+}
