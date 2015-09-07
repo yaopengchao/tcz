4d14dac020f195facf71f29297f23739f29ec872
LancCJ <LancCJ@163.com>
2015-09-07 22:53:20 +0800

    新增UTIL

---
+++ LoginFrame/Util.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace LoginFrame
+{
+    public class Util
+    {
+        public static void setLanguage()
+        {
+            if (LoginRoler.language == Constant.zhCN)
+            {
+                Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("zh-CN");
+            }
+            else if (LoginRoler.language == Constant.En)
+            {
+                Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en");
+            }
+        }
+    }
+}
